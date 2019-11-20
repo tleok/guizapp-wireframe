bb52ddff6906db1741c0a3f76ed449335c32cad1
Tanner Kay <tleok100@gmail.com>
2019-11-19 17:47:45 -0700

    couldnt get the questionComponents to work so I removed it and now it works

--- index.js
+++ index.js
@@ -3,52 +3,52 @@
 //Create a array containing the questions as objects
 const STATES = [
     { //Question 1
-    question: "What is the Capital of Wisconsin?", 
+    question: "What is the Capitol of Wisconsin?", 
     allAnswers: ["Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"],
     answer: "Madison"
     },
     { //Question 2
-    question: "What is the Capital of Alabama?", 
+    question: "What is the Capitol of Alabama?", 
     allAnswers: ["Montgomery", "Anchorage", "Phoenix", "Little Rock", "Sacramento"],
     answer: "Montgomery"
     },
     { //Question 3
-    question: "What is the Capital of South Dakota?", 
+    question: "What is the Capitol of South Dakota?", 
     allAnswers: ["Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier"],
     answer: "Pierre"
     },
     { //Question 4
-    question: "What is the Capital of Delaware?", 
+    question: "What is the Capitol of Delaware?", 
     allAnswers: ["Denver", "Hartford", "Dover", "Tallahassee", "Atlanta"],
     answer: "Dover"
     },
     { //Question 5
-    question: "What is the Capital of Oregon?", 
+    question: "What is the Capitol of Oregon?", 
     allAnswers: ["Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia"],
     answer: "Salem"
     },
     { //Question 6
-    question: "What is the Capital of Illinois?", 
+    question: "What is the Capitol of Illinois?", 
     allAnswers: ["Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines"],
     answer: "Springfield"
     },
     { //Question 7
-    question: "What is the Capital of North Dakota?", 
+    question: "What is the Capitol of North Dakota?", 
     allAnswers: ["Santa Fe", "Albany", "Raleigh", "Bismark", "Columbus"],
     answer: "Bismark"
     },
     { //Question 8
-    question: "What is the Capital of Maine?", 
+    question: "What is the Capitol of Maine?", 
     allAnswers: ["Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis"],
     answer: "Augusta"
     },
     { //Question 9
-    question: "What is the Capital of Nevada?", 
+    question: "What is the Capitol of Nevada?", 
     allAnswers: ["Helena", "Lincoln", "Carson City", "Concord", "Trenton"],
     answer: "Carson City"
     },
     { //Question 10
-    question: "What is the Capital of Mississippi?", 
+    question: "What is the Capitol of Mississippi?", 
     allAnswers: ["Boston", "Lansing", "St. Paul", "Jackson", "Jefferson City"],
     answer: "Jackson"
     },
@@ -58,91 +58,48 @@ const STATES = [
 //Array to store the Score
 const USERRESPONSES = [];
 let currentQuestionIndex = 0;
-let questionComponents = STATES[currentQuestionIndex];
-//Declare Variable for the Answers
-let answerIndex = 0;
-//Testing the display of the question
-console.log(questionComponents.question);
-//testing the display of the Answers
-console.log(questionComponents.allAnswers[answerIndex]);
-//Step through the incrementing of the currentQuestionIndex
+//---------let questionComponents = STATES[currentQuestionIndex];
 
-//function 'nextQuestion' that steps us through the STORE objects and increments the current place holder variable
-  //This function needs to be initiated by the click of the user
-  //This function maybe has a if statement to clarify the item clicked
-    //In this if statement i will need to verify
-function userClick() {
+//function 'userSubmitClick' takes the start, submit, and next question clicks
+function userSubmitClick() {
   $('#js-submission-form').on('click', '.js-submit-btn', event => {
     event.preventDefault();
-    console.log('`userClick` ran');
+    console.log('`userSubmitClick` ran');
     generateTheQuestion();
     incrementQuestionIndex();
     });
 }
-
 //This function generates the question
 function generateTheQuestion() {
   console.log("`generateTheQuestion` ran");
   renderQuestion();
 }
+//Change the Question text to the current Question and invoke the answers
 function renderQuestion(){
-  console.log("`renderQuestionder` ran")
-$('.js-theQuestion').text(questionComponents.question);
+  console.log("`renderQuestion` ran")
+  $('.js-theQuestion').text(STATES[currentQuestionIndex].question);
+  renderAnswers();
 }
+function renderAnswers(){
+  console.log("`renderAnswers` ran")
+$('.js-theQuestion').text(STATES[currentQuestionIndex].question);
+console.log(currentQuestionIndex);
+}
+//Increment the Questions to move through the questions
 function incrementQuestionIndex(){
   currentQuestionIndex++;
   $('.js-currentQuestionNumber').text(currentQuestionIndex);
 }
 //Increment the Answers
-
+function incrementAnswerIndex(){
+  currentAnswerIndex++;
+  STATES[currentQuestionIndex].allAnswers.forEach(function)
+}
 //Render Function onReady
 function quizApp() {
   //generateTheQuestion();
-  userClick();
+  userSubmitClick();
 }
 
 $(quizApp);
-
-
-
-
-
-
-
-
-
-
-//----------------NOT-MINE----------------
-// this generates the question and checks first to see if we are finished if not displays
-function generateQuestion() {
-  if (questionNumber < STORE.length) {
-    return createThing(questionNumber);
-  } else {
-    $('.questionBox').hide();
-    finalScore();
-    $('.questionNumber').text(10);
-  }//----------------NOT-MINE----------------
-}
-//----------------NOT-MINE----------------
-//This function increments the allAnswers to add a button to each
-//creates html for question form
-function createThing(questionIndex) {
-  let formMaker = $(`<form>
-    <fieldset>
-      <legend class="questionText">${STORE[questionIndex].question}</legend>
-    </fieldset>
-  </form>`)
-
-  let fieldSelector = $(formMaker).find('fieldset');
-
-  STORE[questionIndex].answers.forEach(function (answerValue, answerIndex) {
-    $(`<label class="sizeMe" for="${answerIndex}">
-        <input class="radio" type="radio" id="${answerIndex}" value="${answerValue}" name="answer" required>
-        <span>${answerValue}</span>
-      </label>
-      `).appendTo(fieldSelector);
-  });
-  $(`<button type="submit" class="submitButton button"> Submit</button > `).appendTo(fieldSelector);
-  return formMaker;
-}//----------------NOT-MINE----------------
 
