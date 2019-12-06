8b7550213832da0ab8f24c90ccd6214246bdb848
Tanner Kay <tleok100@gmail.com>
2019-12-05 18:52:10 -0700

    added stock photos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const STATES = [
     {
@@ -51,110 +51,103 @@ const STATES = [
     allAnswers: ['Boston', 'Lansing', 'St. Paul', 'Jackson', 'Jefferson City'],
     answer: 'Jackson'
     },
-];
+]
 
-const USERRESPONSES = [];
-let currentQuestionIndex = 0;
-let quizStarted = false;
-let isSubmittingAnswer = false;
-let selectedAnswer = '';
-let quizFinished = false;
+const USERRESPONSES = []
+let currentQuestionIndex = 0
+let quizStarted = false
+let isSubmittingAnswer = false
+let selectedAnswer = ''
+let quizFinished = false
 
-function initEventHandlers() {
-  $('#js-submission-form').on('submit', event => {
-    event.preventDefault();
+function initEventHandlers(){
+  $('#js-submission-form').on('submit', event =>{
+    event.preventDefault()
     if (quizFinished){
-      window.location.reload();
+      window.location.reload()
     }
     if(quizStarted === false){
       quizStarted = true;
-      isSubmittingAnswer = true;
-      showNextQuestion();
+      isSubmittingAnswer = true
+      showNextQuestion()
     } else if(isSubmittingAnswer === true){
-        isSubmittingAnswer = false;
-        showSummary();
+        isSubmittingAnswer = false
+        showSummary()
         if(currentQuestionIndex === STATES.length){
-          $('.js-btn').text('See Final Results');
+          $('.js-btn').text('See Final Results')
         }
     } else{
         if(currentQuestionIndex === STATES.length){
-          showFinalSummary();
-          quizFinished = true;
+          showFinalSummary()
+          quizFinished = true
         } else{
-          isSubmittingAnswer = true;
-          showNextQuestion();
+          isSubmittingAnswer = true
+          showNextQuestion()
         }
     }
-    });
-    $( '.js-answers-list' ).on( 'click', 'input', function() { 
-      selectedAnswer = $('.js-answers-list input:checked' ).val();
-    });
+    })
+    $( '.js-answers-list' ).on( 'click', 'input', function(){ 
+      selectedAnswer = $('.js-answers-list input:checked' ).val()
+    })
 }
 //Show the Next question
-function showNextQuestion() {
+function showNextQuestion(){
   console.log('`renderQuestion` ran')
-  let questionComponents = STATES[currentQuestionIndex];
-  $('.js-theQuestion').text(questionComponents.question);
+  let questionComponents = STATES[currentQuestionIndex]
+  $('.js-theQuestion').text(questionComponents.question)
   STATES[currentQuestionIndex].allAnswers.forEach(function(answerValue, answerIndex){
-    //change div to a plain li
-    $(`<div class='answerContainer js-answerContainer' for='${answerIndex}'>
+    $(`<div for='${answerIndex}'>
           <input class='radio js-radio' type='radio' id='${answerIndex}' value='${answerValue}' name='answer' required>
           <span>${answerValue}</span>
-        </div>`).appendTo('.js-answers-list');
+        </div>`).appendTo('.js-answers-list')
   })
-  $('.js-btn').text('Submit');
-  $('.js-currentQuestionNumber').text(currentQuestionIndex +1);
+  $('.js-btn').text('Submit')
+  $('.js-currentQuestionNumber').text(currentQuestionIndex +1)
 }
 
 function showSummary(){
   console.log('`showSummary` ran')
   $('.js-answers-list').children().remove()
-  let questionComponents = STATES[currentQuestionIndex];
+  let questionComponents = STATES[currentQuestionIndex]
   let correctAnswer = questionComponents.answer;
   if (selectedAnswer === correctAnswer) {
-    $('.js-theQuestion').text('Your answer is correct!');
+    $('.js-theQuestion').text('Your answer is correct!')
         USERRESPONSES.push(true);
-    $('.js-currentScore').text(USERRESPONSES.length);
+    $('.js-currentScore').text(USERRESPONSES.length)
         currentQuestionIndex++;
-        $('.js-btn').text('Next Question');
+        $('.js-btn').text('Next Question')
   } else {
-    $('.js-theQuestion').text('Sorry that`s the wrong answer...keep practicing.');
+    $('.js-theQuestion').text('Sorry that`s the wrong answer...keep practicing.')
         currentQuestionIndex++;
-        $('.js-btn').text('Next Question');
+        $('.js-btn').text('Next Question')
   }
 }
 
-function showFinalSummary() {
+function showFinalSummary(){
   let finalScore = USERRESPONSES.length;
   $('.js-theQuestion').text('Lets See how you did...')
   if(USERRESPONSES.length >= 8){
   $(`<div>
       <span>Great Work</span>
       <span>${finalScore}/10</span>
-     </div>`).appendTo('.js-answers-list');
+     </div>`).appendTo('.js-answers-list')
   } else if (USERRESPONSES.length >=5){
-    //only use single quotes or double quotes. Decide Now
-    //DOnt spend days trying to reverse engineer what they do. do what you know how to do. Look over material if you need.
-    //Also either use ; or dont it doesnt look good to have inconsstencies
-    //also make sure the code looks clean like the divs need to line up and everything needs proper indenting
-    //also make sure you are consistent with spaces or no spaces lik after a function (){} or () {} not both
-    //Quotes need to be meaningful. Dont just add them for debuging. Also NO console.logs.
     $(`<div>
       <span>Good Effort</span>
       <span>${finalScore}/10</span>
-    </div>`).appendTo('.js-answers-list');
+    </div>`).appendTo('.js-answers-list')
   } else {
     $(`<div>
     <span>Keep Practicing</span>
     <span>${finalScore}/10</span>
-  </div>`).appendTo('.js-answers-list');
+  </div>`).appendTo('.js-answers-list')
   }
-  $('.js-btn').text('Restart Quiz');
+  $('.js-btn').text('Restart Quiz')
 }
 
-function quizApp() {
-  initEventHandlers();
+function quizApp(){
+  initEventHandlers()
 }
 
-$(quizApp);
+$(quizApp)
 
